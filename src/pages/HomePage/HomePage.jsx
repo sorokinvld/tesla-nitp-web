12c5ebbd1411a23964e91da1c42559a5bfa08a51
Vladislav Sorokin <sorokinvld@users.noreply.github.com>
2023-03-30 20:28:05 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,8 @@ const HomePage = () => {
   return (
     <div className='pt-16 '>
       <div className='justify-center flex items-center my-[8rem] flex-col text-5xl font-bold'>
-        <h1 className='font-extrabold text-transparent lg:text-8xl md:text-6xl text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Official Website of</h1>
-        <h1 className='font-extrabold text-transparent lg:text-7xl md:text-5xl text-4xl bg-clip-text bg-gradient-to-r from-blue-400 to-sky-600 mt-2'>Tesla NIT Patna</h1>
+        <h1 className='font-extrabold text-transparent lg:text-8xl md:text-6xl text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Deep-mind</h1>
+        <h1 className='font-extrabold text-transparent lg:text-7xl md:text-5xl text-4xl bg-clip-text bg-gradient-to-r from-blue-400 to-sky-600 mt-2'>Студия. Лаборатория. Люди.</h1>
       </div>
     </div>
   )
